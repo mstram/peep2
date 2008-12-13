51ce351b93ea3aa0c14a57b8f4031bc245fc43f1
Mike Stramba <mikestramba@gmail.com>
2008-12-12 19:29:26 -0500

    vegetable cleanup (rm other .rb files .. spec .. etc)

---
+++ sci-fi/lib/vegetable.rb
@@ -0,0 +1,19 @@
+#
+# production version of Vegetable
+#  Fri 12/12/2008
+#  18:57:14.53
+
+class Vegetable
+
+ attr_accessor :name, :color
+
+ def initialize(name,color)
+  @name = name
+  @color = color
+  @born = Time.now
+ end
+
+ def plantit
+ end
+
+end
