a4b05e0df93aa23569df2d5118f12a745e7e89a1
Mike Stramba <mikestramba@gmail.com>
2008-12-12 19:11:13 -0500

    peep2 internal repository

---
+++ sci-fi/lib/time_travel.rb
@@ -0,0 +1,15 @@
+#
+# production version of TimeTravel class
+#  Fri 12/12/2008
+#  18:57:14.53
+
+class TimeTravel
+ attr_accessor :year
+
+ def initialize
+ end
+
+ def travel(to_date)
+ end
+
+end
---
+++ sci-fi/spec/time_travel.spec.rb
@@ -0,0 +1,5 @@
+describe TimeTravel do
+ it "should travel from one year to another"
+ it "should keep age of traveller constant"
+ it "should be fun"
+end
